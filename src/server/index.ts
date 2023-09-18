22ecd1f632fb9f2f646ba08a1d790dd06b0f1875
Caio Fuzatto <caiofuzattom@gmail.com>
2023-09-17 23:00:01 -0300

    add webpack and hydrateRoot

--- src/server/index.ts
+++ src/server/index.ts
@@ -5,24 +5,28 @@ import React from "react";
 import ReactDOMServer from "react-dom/server";
 import express from "express";
 
-import { App } from "./App";
+import { App } from "../client/App";
 
 const app = express();
 
 app.get("/", (req, res) => {
   const appContent = ReactDOMServer.renderToString(React.createElement(App));
-  const indexFile = path.resolve("./index.html");
-
+  const indexFile = path.resolve(`${__dirname}/../src/client/index.html`);
   fs.readFile(indexFile, "utf8", (err, data) => {
     if (err) {
       return res.status(500).send("Não foi possível carregar o app.");
     }
 
     return res.send(
-      data.replace('<div id="root"></div>', `<div id="root">${appContent}</div>`)
+      data.replace(
+        '<div id="root"></div>',
+        `<div id="root">${appContent}</div>`
+      )
     );
   });
 });
+
+app.use(express.static(path.join(__dirname, "public"), { maxAge: "30d" }));
 
 app.listen(8080, () => {
   console.log(`App rodando na porta ${8080}`);
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+
+const clientConfig = {
+  mode: "development",
+  target: "web",
+  entry: "./src/client/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist/public"),
+  },
+};
+
+const serverConfig = {
+  mode: "development",
+  entry: "./src/server/index.ts",
+  target: "node",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
+
+module.exports = [clientConfig, serverConfig];
